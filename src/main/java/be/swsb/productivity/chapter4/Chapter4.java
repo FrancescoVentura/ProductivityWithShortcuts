3bce725499ab57a0882482fb20c5900afce4cef8
Francesco Ventura <Frank@DESKTOP-3JI08SA>
2018-02-20 00:30:01 +0100

    Chapter 4 enjoyed the music

--- src/main/java/be/swsb/productivity/chapter4/Chapter4.java
+++ src/main/java/be/swsb/productivity/chapter4/Chapter4.java
@@ -4,32 +4,23 @@ public class Chapter4 {
 
     /*
      * This is an unnecessary comment, so.... duplicate me :)
-     * Delete all of these lines using ctrl+y
      */
     public Chapter4() {
-    }
-
-    private void _2_snarf() {
-        System.out.println("Lion-O");
     }
 
     private void _1_lionO() {
         System.out.println("Snarf");
     }
 
-    private void _3_cheetara() {
+
+    private void _2_snarf() {
+        System.out.println("Lion-O");
     }
 
+    private void _3_cheetara() {
+
+    }
     private String[] _4_thundercats() {
-        return new String[]{
-                "lion-o",
-                "jaga",
-                "panthro",
-                "tygra",
-                "cheetara",
-                "snarf",
-                "wily kit",
-                "wily kat"
-        };
+        return new String[]{"lion-o", "jaga", "panthro", "tygra", "cheetara", "snarf", "wily kit", "wily kat"};
     }
 }
